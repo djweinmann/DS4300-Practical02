ce506b5ce19d8192ea1815100220472e5f47c794
kennethaldridge <kenneth.r.aldridge@gmail.com>
2025-04-02 10:10:47 -0400

    cleaning up repo

--- scripts/indexall.sh
+++ scripts/indexall.sh
@@ -1,11 +1,11 @@
-# python ingest.py -e nomic-embed-text -d redisstack --indexname nomic
-# python ingest.py -e nomic-embed-text -d chroma --indexname nomic
-# python ingest.py -e nomic-embed-text -d qdrant --indexname nomic
+ python ingest.py -e nomic-embed-text -d redisstack --indexname nomic
+ python ingest.py -e nomic-embed-text -d chroma --indexname nomic
+ python ingest.py -e nomic-embed-text -d qdrant --indexname nomic
 #
 # python ingest.py -e all-minilm -d redisstack --indexname minilm
 # python ingest.py -e all-minilm -d chroma --indexname minilm
 # python ingest.py -e all-minilm -d qdrant --indexname minilm
 
-python ingest.py -e mxbai-embed-large -d redisstack --indexname bread
-python ingest.py -e mxbai-embed-large -d chroma --indexname bread
-python ingest.py -e mxbai-embed-large -d qdrant --indexname bread
+#python ingest.py -e mxbai-embed-large -d redisstack --indexname bread
+#python ingest.py -e mxbai-embed-large -d chroma --indexname bread
+#python ingest.py -e mxbai-embed-large -d qdrant --indexname bread
